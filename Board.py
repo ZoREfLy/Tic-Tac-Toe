cad2965bae2db75592b031fe1a1e7b2c8ae65504
Zorefly <zorefly@zoreflys-mbp.attlocal.net>
2019-07-03 12:20:47 -0700

    change format

--- Board.py
+++ Board.py
@@ -5,176 +5,176 @@ from Brain import Brain
 
 
 class Board:
-	"""
-	This object allows player(s) to take actions,
-	remembers the current state of game, remembers
-	and prints the current state of game
-	"""
-	def __init__(self):
-		self.done = False
-		self.winner = None
-		self.first_player = True
-		self.state = np.zeros((3, 3))
-		self.player = {3: 'Player 1', -3: 'Player 2'}
-		self.operations = {'Q': (0, 0), 'W': (0, 1), 'E': (0, 2),
-						'A': (1, 0), 'S': (1, 1), 'D': (1, 2),
-						'Z': (2, 0), 'X': (2, 1), 'C': (2, 2)}
+    """
+    This object allows player(s) to take actions,
+    remembers the current state of game, remembers
+    and prints the current state of game
+    """
+    def __init__(self):
+        self.done = False
+        self.winner = None
+        self.first_player = True
+        self.state = np.zeros((3, 3))
+        self.player = {3: 'Player 1', -3: 'Player 2'}
+        self.operations = {'Q': (0, 0), 'W': (0, 1), 'E': (0, 2),
+                        'A': (1, 0), 'S': (1, 1), 'D': (1, 2),
+                        'Z': (2, 0), 'X': (2, 1), 'C': (2, 2)}
 
-	def is_done(self):
-		# detect if there are 3 same symbols in the same column
-		for col in range(0, 3):
-			total = sum(self.state[:, col])
-			if total == 3 or total == -3:
-				self.done = True
-				self.winner = self.player[total]
-				return
+    def is_done(self):
+        # detect if there are 3 same symbols in the same column
+        for col in range(0, 3):
+            total = sum(self.state[:, col])
+            if total == 3 or total == -3:
+                self.done = True
+                self.winner = self.player[total]
+                return
 
-		# detect if there are 3 same symbols in the same row
-		for row in range(0, 3):
-			total = sum(self.state[row, :])
-			if total == 3 or total == -3:
-				self.done = True
-				self.winner = self.player[total]
-				return
+        # detect if there are 3 same symbols in the same row
+        for row in range(0, 3):
+            total = sum(self.state[row, :])
+            if total == 3 or total == -3:
+                self.done = True
+                self.winner = self.player[total]
+                return
 
-		# detect if there are 3 same symbols in diagonal (up-left to bottom-right)
-		total = 0
-		for i in range(0, 3):
-			total += self.state[i, i]
-		if total == 3 or total == -3:
-			self.done = True
-			self.winner = self.player[total]
-			return
+        # detect if there are 3 same symbols in diagonal (up-left to bottom-right)
+        total = 0
+        for i in range(0, 3):
+            total += self.state[i, i]
+        if total == 3 or total == -3:
+            self.done = True
+            self.winner = self.player[total]
+            return
 
-		# detect if there are 3 same symbols in diagonal (bottom-left to up-right)
-		total = self.state[0, 2] + self.state[1, 1] + self.state[2, 0]
-		if total == 3 or total == -3:
-			self.done = True
-			self.winner = self.player[total]
-			return
+        # detect if there are 3 same symbols in diagonal (bottom-left to up-right)
+        total = self.state[0, 2] + self.state[1, 1] + self.state[2, 0]
+        if total == 3 or total == -3:
+            self.done = True
+            self.winner = self.player[total]
+            return
 
-		if 0 not in self.state:
-			self.done = True
+        if 0 not in self.state:
+            self.done = True
 
-	def reset(self):
-		self.done = False
-		self.winner = None
-		self.state[:, :] = 0
-		self.first_player = True
+    def reset(self):
+        self.done = False
+        self.winner = None
+        self.state[:, :] = 0
+        self.first_player = True
 
-	def step(self, i, j, player=None):
-		if not player:
-			if self.first_player:
-				self.state[i, j] = 1
-			else:
-				self.state[i, j] = -1
-		else:
-			self.state[i, j] = player
+    def step(self, i, j, player=None):
+        if not player:
+            if self.first_player:
+                self.state[i, j] = 1
+            else:
+                self.state[i, j] = -1
+        else:
+            self.state[i, j] = player
 
-		self.is_done()
+        self.is_done()
 
-	def __str__(self):
-		row = []
-		for i in np.nditer(self.state):
-			if i == -1:
-				row += [' X ']
-			elif i == 0:
-				row += ['   ']
-			else:
-				row += [' O ']
-		return '{}|{}|{}\n-----------\n{}|{}|{}\n-----------\n{}|{}|{}\n'.format(*row)
+    def __str__(self):
+        row = []
+        for i in np.nditer(self.state):
+            if i == -1:
+                row += [' X ']
+            elif i == 0:
+                row += ['   ']
+            else:
+                row += [' O ']
+        return '{}|{}|{}\n-----------\n{}|{}|{}\n-----------\n{}|{}|{}\n'.format(*row)
 
-	def train(self):
-		print('Training...')
-		p1 = p2 = t = 0
-		self.AI_1 = Brain(1)
-		self.AI_2 = Brain(-1)
+    def train(self):
+        print('Training...')
+        p1 = p2 = t = 0
+        self.AI_1 = Brain(1)
+        self.AI_2 = Brain(-1)
 
-		for _ in range(9000):
-			self.reset()
-			# print(self)
+        for _ in range(9000):
+            self.reset()
+            # print(self)
 
-			while not self.done:
-				if self.first_player:
-					player = self.AI_1
-				else:
-					player = self.AI_2
+            while not self.done:
+                if self.first_player:
+                    player = self.AI_1
+                else:
+                    player = self.AI_2
 
-				i, j, symbol = player.choose_action(self.state)
-				self.step(i, j, symbol)
+                i, j, symbol = player.choose_action(self.state)
+                self.step(i, j, symbol)
 
-				# print(self)
+                # print(self)
 
-				self.first_player = not self.first_player
+                self.first_player = not self.first_player
 
-			# self.game_over()
+            # self.game_over()
 
-			if self.winner == 'Player 1':
-				p1 += 1
-				self.AI_1.train(1)
-				self.AI_2.train(0)
-			elif self.winner == 'Player 2':
-				p2 += 1
-				self.AI_1.train(0)
-				self.AI_2.train(1)
-			else:
-				t += 1
-				self.AI_1.train(0.5)
-				self.AI_2.train(0.5)
+            if self.winner == 'Player 1':
+                p1 += 1
+                self.AI_1.train(1)
+                self.AI_2.train(0)
+            elif self.winner == 'Player 2':
+                p2 += 1
+                self.AI_1.train(0)
+                self.AI_2.train(1)
+            else:
+                t += 1
+                self.AI_1.train(0.5)
+                self.AI_2.train(0.5)
 
-		print('P1: {}\nP2: {}\nT: {}'.format(p1, p2, t))
-		self.AI_1.save('p1.dat')
-		self.AI_2.save('p2.dat')
+        print('P1: {}\nP2: {}\nT: {}'.format(p1, p2, t))
+        self.AI_1.save('p1.dat')
+        self.AI_2.save('p2.dat')
 
-	def play(self):
-		self.reset()
-		mode = int(input("0 for Human-AI, 1 for Human-Human: "))
+    def play(self):
+        self.reset()
+        mode = int(input("0 for Human-AI, 1 for Human-Human: "))
 
-		if mode in range(2):
-				print("Instruction:")
-				print(' Q | W | E \n-----------\n A | S | D \n-----------\n Z | X | C \n')
-				print(self)
+        if mode in range(2):
+                print("Instruction:")
+                print(' Q | W | E \n-----------\n A | S | D \n-----------\n Z | X | C \n')
+                print(self)
 
-				if mode == 0:
-					if os.path.exists('p2.dat'):
-						self.AI_2 = Brain(-1)
-						self.AI_2.load('p2.dat')
-					else:
-						self.train()
-						self.reset()
+                if mode == 0:
+                    if os.path.exists('p2.dat'):
+                        self.AI_2 = Brain(-1)
+                        self.AI_2.load('p2.dat')
+                    else:
+                        self.train()
+                        self.reset()
 
-					while not self.done:
-						if self.first_player:
-							move = input("Your move: ")
-							self.step(*self.operations[move.upper()])
-							self.first_player = not self.first_player
-							print(self)
-						else:
-							i, j, symbol = self.AI_2.choose_action(self.state, True)
-							self.step(i, j, symbol)
-							self.first_player = not self.first_player
-							print(self)
-				else:
-					while not self.done:
-						move = input("Your move: ")
+                    while not self.done:
+                        if self.first_player:
+                            move = input("Your move: ")
+                            self.step(*self.operations[move.upper()])
+                            self.first_player = not self.first_player
+                            print(self)
+                        else:
+                            i, j, symbol = self.AI_2.choose_action(self.state, True)
+                            self.step(i, j, symbol)
+                            self.first_player = not self.first_player
+                            print(self)
+                else:
+                    while not self.done:
+                        move = input("Your move: ")
 
-						self.step(*self.operations[move.upper()])
+                        self.step(*self.operations[move.upper()])
 
-						self.first_player = not self.first_player
+                        self.first_player = not self.first_player
 
-						print(self)
-						# print(self.state)
+                        print(self)
+                        # print(self.state)
 
-				self.game_over()
+                self.game_over()
 
-	def game_over(self):
-		print("Game Over!")
-		if not self.winner:
-			print("Tire!")
-		else:
-			print("The winner is {}!".format(self.winner))
+    def game_over(self):
+        print("Game Over!")
+        if not self.winner:
+            print("Tire!")
+        else:
+            print("The winner is {}!".format(self.winner))
 
 if __name__ == '__main__':
-	board = Board()
-	# board.train()
-	board.play()
+    board = Board()
+    # board.train()
+    board.play()
--- Brain.py
+++ Brain.py
@@ -2,85 +2,85 @@ import random
 import numpy as np
 
 class Brain:
-	"""
-	This object remembers each state and its probability to result
-	"""
-	def __init__(self, symbol, learning_rate=0.5, epsilon=0.7):
-		self.moves = []
-		self.memory = {}
-		self.symbol = symbol
-		self.epsilon = epsilon
-		self.learning_rate = learning_rate
+    """
+    This object remembers each state and its probability to result
+    """
+    def __init__(self, symbol, learning_rate=0.5, epsilon=0.7):
+        self.moves = []
+        self.memory = {}
+        self.symbol = symbol
+        self.epsilon = epsilon
+        self.learning_rate = learning_rate
 
-	def reset(self):
-		del self.moves
-		self.moves = []
+    def reset(self):
+        del self.moves
+        self.moves = []
 
-	def choose_action(self, state, full=None):
-		candidate_states_hashs = []
-		candidate_states_values = []
-		candidate_states_positions = []
+    def choose_action(self, state, full=None):
+        candidate_states_hashs = []
+        candidate_states_values = []
+        candidate_states_positions = []
 
-		# find all possible moves
-		for i in range(0, 3):
-			for j in range(0, 3):
-				if state[i, j] == 0:
-					candidate_states_positions.append((i, j)) # remember the position of the possible move
-					state[i, j] = self.symbol # temporarily change the state
-					
-					hash_codes = state.tobytes() # calculate the hash code of the temporary state
-					candidate_states_hashs.append(hash_codes) # remember the hash code
+        # find all possible moves
+        for i in range(0, 3):
+            for j in range(0, 3):
+                if state[i, j] == 0:
+                    candidate_states_positions.append((i, j)) # remember the position of the possible move
+                    state[i, j] = self.symbol # temporarily change the state
+                    
+                    hash_codes = state.tobytes() # calculate the hash code of the temporary state
+                    candidate_states_hashs.append(hash_codes) # remember the hash code
 
-					# if the temporary state is not in memory, save it and set the value to default (0.5)
-					if hash_codes not in self.memory:
-						self.memory[hash_codes] = 0.5
-					candidate_states_values.append(self.memory[hash_codes]) # remember the value of the temporary state
+                    # if the temporary state is not in memory, save it and set the value to default (0.5)
+                    if hash_codes not in self.memory:
+                        self.memory[hash_codes] = 0.5
+                    candidate_states_values.append(self.memory[hash_codes]) # remember the value of the temporary state
 
-					state[i, j] = 0 # change back to original state
-		
-		if np.random.rand() < self.epsilon or full: # choose an action according to action values
+                    state[i, j] = 0 # change back to original state
+        
+        if np.random.rand() < self.epsilon or full: # choose an action according to action values
 
-			# remember all actions that have max value
-			indices = [i for i, v in enumerate(candidate_states_values) if v == max(candidate_states_values)]
-			
-			if len(indices) == 1: # only one action has the highest value
-				index = indices[0]
-				action = candidate_states_positions[index]
-				self.remember(candidate_states_hashs[index])
-				return action[0], action[1], self.symbol
-			else: # multiple actions have the highest value
-				index = random.choice(indices) # randomly choose one of them
-				action = candidate_states_positions[index]
-				self.remember(candidate_states_hashs[index])
-				return action[0], action[1], self.symbol
-		else: # randomly choose an action by avoiding local minima
-			index = random.randint(0, len(candidate_states_hashs)-1)
-			action = candidate_states_positions[index]
-			self.remember(candidate_states_hashs[index])
-			return action[0], action[1], self.symbol
+            # remember all actions that have max value
+            indices = [i for i, v in enumerate(candidate_states_values) if v == max(candidate_states_values)]
+            
+            if len(indices) == 1: # only one action has the highest value
+                index = indices[0]
+                action = candidate_states_positions[index]
+                self.remember(candidate_states_hashs[index])
+                return action[0], action[1], self.symbol
+            else: # multiple actions have the highest value
+                index = random.choice(indices) # randomly choose one of them
+                action = candidate_states_positions[index]
+                self.remember(candidate_states_hashs[index])
+                return action[0], action[1], self.symbol
+        else: # randomly choose an action by avoiding local minima
+            index = random.randint(0, len(candidate_states_hashs)-1)
+            action = candidate_states_positions[index]
+            self.remember(candidate_states_hashs[index])
+            return action[0], action[1], self.symbol
 
-	def remember(self, hash_code):
-		self.moves.append(hash_code)
+    def remember(self, hash_code):
+        self.moves.append(hash_code)
 
-	def train(self, result):
-		self.moves.reverse()
-		self.memory[self.moves[0]] = next_state_value = result
+    def train(self, result):
+        self.moves.reverse()
+        self.memory[self.moves[0]] = next_state_value = result
 
-		for i in self.moves[1:]:
-			self.memory[i] += self.learning_rate * (next_state_value - self.memory[i])
-			next_state_value = self.memory[i]
+        for i in self.moves[1:]:
+            self.memory[i] += self.learning_rate * (next_state_value - self.memory[i])
+            next_state_value = self.memory[i]
 
-		self.reset()
-		# print(self.memory)
+        self.reset()
+        # print(self.memory)
 
-	def save(self, file_name):
-		with open(file_name, 'w') as f:
-			for key, val in self.memory.items():
-				f.write('{} {}\n'.format(key, val))
+    def save(self, file_name):
+        with open(file_name, 'w') as f:
+            for key, val in self.memory.items():
+                f.write('{} {}\n'.format(key, val))
 
-	def load(self, file_name):
-		with open(file_name, 'r') as f:
-			for line in f:
-				key, val = line.split()
-				self.memory[key] = val
-		
+    def load(self, file_name):
+        with open(file_name, 'r') as f:
+            for line in f:
+                key, val = line.split()
+                self.memory[key] = val
+        
